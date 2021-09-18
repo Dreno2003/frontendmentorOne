f0d8c4c38e15c4e08c35065f28348cfb80c61336
Idorenyin Akaininyene <=>
2021-09-18 07:41:36 +0100

    fixed mobile and desktop desing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,28 @@ module.exports = {
         'heroimg-mobile': 'url(public/images/bg-intro-mobile.svg)',
         'herodimg-desktop':"url('/public/images/bg-intro-desktop.svg')"
 
-      })
+      }),
+
+      width: {
+        '1/02': '21em',
+        '1/03': '62.65vw',
+        '1/04': '41vw',
+        '1/05': '88.6vw',
+        '1/06': '76vw',
+        '1/07': '33vw',
+        '1/08': '91vw', //mainC
+        '1/09': '55%',
+        '1/10': '52vw',
+        '1/00': '15em'
+        '2/01': '83vw',
+        '2/02': '23vw'
+      },
+
+      margin: {
+        '-1/01': '-17.5vh'
+      }
+
+      
     },
   },
   variants: {
